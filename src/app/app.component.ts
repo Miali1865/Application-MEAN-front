eb685233bcaf599e19e7a06788c6998eaa385c93
Miali1865 <mialivola@impirate>
2025-03-15 20:46:50 +0300

    first impression with footer and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,30 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [HeaderComponent, FooterComponent, RouterOutlet]
 })
-export class AppComponent { }
+
+
+export class AppComponent implements OnInit {
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit() {
+    this.updateTitle();
+  }
+
+  updateTitle() {
+    const path = window.location.pathname;
+    let pageTitle = '';
+
+    if (path.includes('contact')) {
+      pageTitle = 'Contact';
+    } else if (path.includes('bienvenue')) {
+      pageTitle = 'Bienvenue';
+    } else if (path.includes('about')) {
+      pageTitle = 'À propos';
+    } else if (path === '/') {
+      pageTitle = 'Page d\'accueil';
+    }
+
+    this.titleService.setTitle(`${pageTitle} - MEKANIKS`);
+  }
+}

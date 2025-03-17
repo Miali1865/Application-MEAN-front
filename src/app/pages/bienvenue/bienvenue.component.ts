571cbb61de7c15af09bd19d2f954486db71359ec
balita <balitatsiory@gmail.com>
2025-03-17 19:52:39 +0300

    title acceuil

--- src/app/pages/bienvenue/bienvenue.component.ts
+++ src/app/pages/bienvenue/bienvenue.component.ts
@@ -1,6 +1,5 @@
 import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bienvenue',
@@ -11,28 +10,10 @@ import {Title} from '@angular/platform-browser';
 })
 export class BienvenueComponent implements OnInit {
 
-  constructor(private titleService: Title) {}
 
   ngOnInit() {
-    this.updateTitle();
   }
 
-  updateTitle() {
-    const path = window.location.pathname;
-    let pageTitle = '';
-
-    if (path.includes('contact')) {
-      pageTitle = 'Contact';
-    } else if (path.includes('bienvenue')) {
-      pageTitle = 'Bienvenue';
-    } else if (path.includes('about')) {
-      pageTitle = 'À propos';
-    } else if (path === '/') {
-      pageTitle = 'Page d\'accueil';
-    }
-
-    this.titleService.setTitle(`${pageTitle} - MEKANIKS`);
-  }
 
 
 }

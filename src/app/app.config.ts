c3001feb295b8fff9e43019b9cbad540cce6106d
balita <balitatsiory@gmail.com>
2025-03-18 15:52:14 +0300

    add loader spinner service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,23 +1,21 @@
-import {importProvidersFrom, LOCALE_ID} from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { LOCALE_ID} from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ContactComponent } from './contact/contact.component';
-import { PackListComponent } from './components/pack-list/pack-list.component';
-import { provideHttpClient } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, provideHttpClient, withInterceptors} from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import {MessageService} from "primeng/api";
 
 import { routes } from './app.routes';
 
-import { ApplicationConfig } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
+import {LoaderspinnerService} from './services/loaderspinner/loaderspinner.service';
+import {loaderspinnerInterceptor} from './inetrceptors/loaderspinner/loaderspinner.interceptor';
 
 export const appConfig = {
   providers: [
     MessageService,
-
+    LoaderspinnerService,
     provideAnimationsAsync(), // Configuration recommandée pour Angular 19
     providePrimeNG({
       theme: {
@@ -25,7 +23,9 @@ export const appConfig = {
       }
     }),
     { provide: LOCALE_ID, useValue: 'fr-FR' },
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([loaderspinnerInterceptor])
+    ),
     provideRouter(routes),
   ],
   imports: [RouterModule]
